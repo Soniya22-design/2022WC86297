caf7f800b0cd1783d9d200693e6b29ada21da0e6
Soniya Tiwari <2022wc86297@wilp.bits-pilani.ac.in>
2026-09-25 10:00:20 +0530

    Updated App.java

--- studentapp/src/main/java/com/bits/wilp/App.java
+++ studentapp/src/main/java/com/bits/wilp/App.java
@@ -1,13 +1,12 @@
 package com.bits.wilp;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("Name : Soniya Tiwari");
+        System.out.println("BITS ID : 2022WC86297");
+        System.out.println("DevOps Lab M4 and M5");
+
     }
 }
